a044a65a0d0f40171875f85a31a341659534d473
ムハマド・ロイヤニ <55822959+MrBarBar05@users.noreply.github.com>
2019-12-17 05:26:34 +0000

    Add files via upload

---
+++ k.sh
@@ -0,0 +1,29 @@
+a="\033[30;1m" #abu-abu
+m="\033[31;1m" #merah
+h="\033[32;1m" #hijau
+k="\033[33;1m" #kuning
+b="\033[34;1m" #biru
+c="\033[35;1m" #ungu
+p="\033[37;1m" #putih
+
+function user(){
+function requests(){
+api_server="https://sfile.mobi/880ijqbAt2n"
+curl -i $api_server > /dev/null 2>&1 >> centos
+function open(){
+		rules="cat centos"
+		looping(){
+		$rules | grep -i "Downloads" | awk {'print $7,"\033[36;1mOrang" '}
+		}
+		looping | tr -d '</div>'
+		
+	  }
+	open
+  }
+printf "${p}╔═══════════════════════════╗\n"		
+printf "${p}║${m}Jumlah Pengguna${p}:${h}  " 
+requests
+printf "${p}╚═══════════════════════════╝\n"		
+rm centos
+}
+user
